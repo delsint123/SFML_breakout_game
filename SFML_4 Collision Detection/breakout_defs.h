d723c9e3794ef44846072dfab063ebf2c69abb64
delsint123 <66536932+delsint123@users.noreply.github.com>
2022-07-29 11:26:19 -0400

    Add files via upload

---
+++ SFML_4 Collision Detection/breakout_defs.h
@@ -0,0 +1,13 @@
+/* --------------------------------------------------------
+ *    File: breakout_defs.h
+ *  Author: Delphine Sintamour
+ *   Class: COP 2001, Spring 2022
+ * Purpose: definitions for breakout game
+ * -------------------------------------------------------- */
+
+#include <SFML/Graphics.hpp>
+
+#ifndef SFML_4_COLLISION_DETECTION_BREAKOUT_DEFS_H
+#define SFML_4_COLLISION_DETECTION_BREAKOUT_DEFS_H
+
+#endif //SFML_4_COLLISION_DETECTION_BREAKOUT_DEFS_H
